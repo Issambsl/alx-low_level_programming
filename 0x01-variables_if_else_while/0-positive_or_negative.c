85a813f8a2761dc4ce821cdfb7e89563d837fe30
Boussalla Issam <boussalla.issa@gmail.com>
2023-06-18 10:19:59 -0700

    my first program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#incloude <stdio.h>
+#incloude <stdlib.h>
+int main()
+{
+	float N;
+	printf("enter a random number:"\n);
+		scanf("%f",N);
+		if(N>0);
+			printf("is positive"\n);
+		else if (N=0);
+			printf("is zero"\n);
+		else
+			printf("is negative"\n);
+		return(0)
+}
