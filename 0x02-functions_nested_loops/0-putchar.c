d869996972187be9592acc8fc511431bcd59d711
Boussalla Issam <boussalla.issa@gmail.com>
2023-06-20 15:26:27 -0700

    my first program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - check the code
+ * description: prints _putchar
+ * Return: Always 0.
+ */
 
+int main(void)
+{
+printf("_putchar\n");
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+return (0);
+}
